21c7610f1b0748b05e934ad85c9d7aa3d336bd2e
Paritosh Panda <paritosh.panda_ug23@ashoka.edu.in>
2021-12-10 11:36:15 +0530

    removed unnecessary files

--- movieflix/settings.py
+++ movieflix/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / "subdir".
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = "django-insecure-(jl5y5+3c_c#qg!)l8k(!%g70!#f1#sqe5(*uey3*$%_+%dfa("
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG")
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
